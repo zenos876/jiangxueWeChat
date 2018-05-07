f50d056465b9a3bf6acfd4de5f59c76e84c560c6
zenos876 <876004947@qq.com>
2018-05-07 16:55:52 +0800

    a
    a

---
+++ src/settings.52304.js
@@ -0,0 +1,521 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "Hook",
+        "Gold",
+        "Stone",
+        "Wall",
+        "Bone",
+        "Drill",
+        "Tnt",
+        "Shoes"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            false
+        ],
+        [
+            false,
+            true,
+            false,
+            false
+        ],
+        [
+            false,
+            true,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            true,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "310SnM8KVNdaHrSrZ+PXTz": [
+                "res/audio/Bgsound1.mp3",
+                1
+            ],
+            "d8hNVMGStHMJXvpmHOMX5G": [
+                "res/audio/Bgsound2.mp3",
+                1
+            ],
+            "14Nx9Fj91C9rEXSDg2KdaS": [
+                "res/audio/Bgsound3.mp3",
+                1
+            ],
+            "b0MXWza7tI5pnXOhHyAD7w": [
+                "res/audio/Bgsound4.mp3",
+                1
+            ],
+            "34yM1sTkVE54LFhc5sI5KG": [
+                "res/audio/Boomb.mp3",
+                1
+            ],
+            "719TiTKXxO242ruUBUZ+9a": [
+                "res/audio/Btnsound.mp3",
+                1
+            ],
+            "00j1t96URGvrtMEhUPusBd": [
+                "res/audio/Failsound.mp3",
+                1
+            ],
+            "23wOcyDjJLa4reQ+mpn/07": [
+                "res/audio/Levelup.mp3",
+                1
+            ],
+            "24Vwsc58RE8bBpEgISRtlX": [
+                "res/audio/Startsound.mp3",
+                1
+            ],
+            "39YO0Bd+1MZ5hqQQIOrK2p": [
+                "res/audio/normal-value.mp3",
+                1
+            ],
+            "67vwOMJY9PALud2nK0gqj5": [
+                "res/audio/score1.mp3",
+                1
+            ],
+            "eajm1OrztBJJqpKkOc4zmv": [
+                "res/gamesellect/menu0.png",
+                0
+            ],
+            "f9AHkcqzVEGZSY6wq3lTVW": [
+                "res/gamesellect/menu1.png",
+                0
+            ],
+            "ffXVJioW1L2KVoPwI4qK29": [
+                "res/gamesellect/menu2.png",
+                0
+            ],
+            "23o1MUGw9OC63AK/OpJUdz": [
+                "res/gamesellect/menu3.png",
+                0
+            ],
+            "dcUlKZh7VOJJroOrzWKZyq": [
+                "res/gamesellect/tu0.png",
+                0
+            ],
+            "b45KCFeiNGyY+gqUvLDDrR": [
+                "res/gamesellect/tu1.png",
+                0
+            ],
+            "339/3zWQNLlYxkb0u5tI97": [
+                "res/gamesellect/tu2.png",
+                0
+            ],
+            "6fiIKgZspKvpJJYF4bgwZI": [
+                "res/gamesellect/tu3.png",
+                0
+            ],
+            "0a7U03JRxKpKSfKj8wkQHn": [
+                "res/gamesellect/xiangyou1.png",
+                0
+            ],
+            "92xvmGKkVElJoJynZINdve": [
+                "res/gamesellect/xiangyou2.png",
+                0
+            ],
+            "26XW6q/ldBy6YwPGXzGZTv": [
+                "res/gamesellect/xiangzuo1.png",
+                0
+            ],
+            "9blESknPZN9ZeE+dNjyYQE": [
+                "res/gamesellect/xiangzuo2.png",
+                0
+            ],
+            "75dMKKrjJO46AQSBXEIjb1": [
+                "res/gamesellect/xuanguanbeijing.png",
+                0
+            ],
+            "cd9ByfUo5BnKrnw/ArHeZU": [
+                "res/gamestart/background.png",
+                0
+            ],
+            "e7UCLj8Q1Mwrygwswn/HPF": [
+                "res/gamestart/fanhui1.png",
+                0
+            ],
+            "180IMY7itFKYYBp9zYOpWY": [
+                "res/gamestart/fanhui2.png",
+                0
+            ],
+            "9bnsx0hSpAfYuSvdelBEUa": [
+                "res/gamestart/kaishi1.png",
+                0
+            ],
+            "64rvaMUPlCx6miP3C1IIiS": [
+                "res/gamestart/kaishi2.png",
+                0
+            ],
+            "1dzPZTKxhOWrAJRW31ZsfY": [
+                "res/gamestart/rope.png",
+                0
+            ],
+            "05bOkSiTFAMZ8Fzme9un0N": [
+                "res/pre/fish-0.png",
+                0
+            ],
+            "45KBujlMZGQJwEPFBFaM7K": [
+                "res/pre/fish-1.png",
+                0
+            ],
+            "9cp6vU1mJHeI1D/P5Lbu5j": [
+                "res/pre/fish-2.png",
+                0
+            ],
+            "61s3MgsKBGsZ8Dieo7jqLv": [
+                "res/pre/fish-3.png",
+                0
+            ],
+            "dfQ6eLeMtDwYFiR4j+HKuC": [
+                "res/pre/fish-4.png",
+                0
+            ],
+            "598SotPtJCIZ6X2eBkIbJJ": [
+                "res/pre/fish-5.png",
+                0
+            ],
+            "cbutSNmlxAQLlROarC4RAL": [
+                "res/pre/gem.png",
+                0
+            ],
+            "56KjfpVGFLhasK72kjUeYi": [
+                "res/pre/shoes-0.png",
+                0
+            ],
+            "4btb1hP59PXYfxXcpoNG+s": [
+                "res/pre/shoes-1.png",
+                0
+            ],
+            "deiQtk6MxLQ48jYnlo5x/E": [
+                "res/pre/stone-0.png",
+                0
+            ],
+            "5fq1QG/jVAuYbFktgkP69V": [
+                "res/pre/stone-1.png",
+                0
+            ],
+            "f0Unr6HbBAo6qEziYm7dw4": [
+                "res/pre/stone-2.png",
+                0
+            ],
+            "1acLsaaSlM0pRblkPTDR2d": [
+                "res/pre/stone-3.png",
+                0
+            ],
+            "48LHhuGh5DQL7Txocw5iYR": [
+                "res/tustart/Btnfram.png",
+                0
+            ],
+            "cchLzde5pB1JnM6KJIbkk3": [
+                "res/tustart/Btnfram2.png",
+                0
+            ],
+            "76hbgXaZtFEoktE29hbd3G": [
+                "res/tustart/bg-0.png",
+                0
+            ],
+            "f00VcymWdFeKsFkorai2yT": [
+                "res/tustart/bg-1.png",
+                0
+            ],
+            "61tnhBxTRBO7XfFlrvrT/T": [
+                "res/tustart/bg-2.png",
+                0
+            ],
+            "96vYgyETBMeKFUtHmu8szi": [
+                "res/tustart/bg-3.png",
+                0
+            ],
+            "17/FVP0t1LZoBLsMnUJJoO": [
+                "res/tustart/boom.png",
+                0
+            ],
+            "cdE+l9VMxMv7UQ7JeZ5ApH": [
+                "res/tustart/framework1.png",
+                0
+            ],
+            "bbx/E5jVJDcb02I8ZEkfAL": [
+                "res/tustart/hook.png",
+                0
+            ],
+            "0cHmsphBRAL5skYYwlZZiw": [
+                "res/tustart/menu.png",
+                0
+            ],
+            "87fgM8DWpEarx6LF9DV965": [
+                "res/tustart/menu2.png",
+                0
+            ],
+            "9foJe5WUNHN5BEsWNgBtok": [
+                "res/tustart/pause.png",
+                0
+            ],
+            "2fWJb7hhxDKp1kCDatM0yl": [
+                "res/tustart/pause2.png",
+                0
+            ],
+            "08KS4gfzdIzaptOO2W87Z+": [
+                "res/tustart/progress.png",
+                0
+            ],
+            "3aJQCXkANPApckVoYJK1kG": [
+                "res/tustart/restart.png",
+                0
+            ],
+            "99gWlLE65OKa5djfthkuZT": [
+                "res/tustart/restart2.png",
+                0
+            ],
+            "14nntshyZMoZ4vnt2OqL0m": [
+                "res/tustart/ship1.png",
+                0
+            ],
+            "d6ZhFnXEZMxKLvezaVvu4U": [
+                "res/tustart/ship2.png",
+                0
+            ],
+            "41DWq4cPpNtJTZMXbc1isO": [
+                "res/tustart/xiegan.png",
+                0
+            ]
+        },
+        internal: {
+            "6eBWFz0oVHPLIGQKf/9Thu": [
+                "image/default_sprite.png",
+                0
+            ],
+            "02delMVqdBD70a/HSD99FK": [
+                "image/default_sprite_splash.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D",
+        "cc.AudioClip"
+    ],
+    launchScene: "db://assets/Scene/Index.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/Index.fire",
+            uuid: 1
+        },
+        {
+            url: "db://assets/Scene/GameSellect.fire",
+            uuid: 0
+        },
+        {
+            url: "db://assets/Scene/Game.fire",
+            uuid: 2
+        }
+    ],
+    packedAssets: {
+        "04defaae1": [
+            "04lWW1gPRPS65MtmuD4oPu",
+            "0a9zMGCWtJi6j/ruq1Y4+Z",
+            "1fBHT/K75Gdp836FG5Kix4",
+            "28A9lVFFBBYJWnXBBz3fPY",
+            "3fGvIYlO1NkbLiazL+ot/y",
+            "41vNOiJ9tEm4XbRqa6jMuG",
+            "4btGiFqW1DxJsBEZEXHvaa",
+            "54WDeFnn9OX4S7p9/kuSrt",
+            "99sP7I6yFG3Y3bo0K4GGxN",
+            "9azmxXm6RLyLppgOHzV1ko",
+            "b9ujZE8ppNNYxMENYt/oSK",
+            "c8Eqa4zMlK+Ipk00vXxY+V",
+            0,
+            "e5mcdWVt9Mg5SZKBmx+F0I",
+            "edbpCaQ8hH4b1TbsVRTSz7",
+            "f60DlRnqVOmaR6M9KuBcSc"
+        ],
+        "0f0998c25": [
+            "18zFUs2JpG9bN9AXEyUJBA",
+            1,
+            "67d0DOuCVFQbAeRahvsKGO",
+            "a2yiAFidJDkLzF1zoliYR/",
+            "b9VYVuWDpI1IhSKRvKrK5c"
+        ],
+        "0f4cb3be0": [
+            "087ihCb3lLSrOwr1yPsPQ6",
+            "0bkbjMMfJNk7oka9BLho7Z",
+            "0bm+qwLm9L3YJcCjKLBTcl",
+            "14hrwAum1BnZJ39i2a3nn3",
+            "17zpBwW91KSJ5MPkINslRr",
+            "24smnltKFDSpWsuMoJKcd8",
+            "28X0+Fam1BL59o11A7ERFR",
+            "29i70n8JZEWbroLoFneNxK",
+            "2c/vJIzpxIAK2oZ1QxY44D",
+            "2eNkWSNFlPRKDc1nIvIBtH",
+            "36BT2qIeZAQYej6c//Rt7a",
+            "3efNYsMexCmZjOdYArSIRd",
+            "45BGRoStFMoa9Z0tkBXNu5",
+            "47dMe+KeZCiYU/T3g4eV0O",
+            "4ajOez2zZBx4PU5E2kGl3L",
+            "4c8McCUmRGYYoTlrbNwOwz",
+            "4ei2MLxEBDI5PS9KpstjCF",
+            "4e5cLoAnRKH5OMeoikV86M",
+            "53yQKN6R9P9aTijDFyd6dl",
+            "66UAUmIFRPEYJNYsyVBjsN",
+            "673N8UQUNAi7H7wd961UXx",
+            2,
+            "74sBYYpFdOMaUkMMZkbRDO",
+            "7496v6ioBEi4wubDB9UoSV",
+            "76lS9tjyFDyqZxBYdhJJ9/",
+            "77XBC9lk9LuZwr4Fw6jSzE",
+            "7bvJ12CutJ34ANE7L+01D9",
+            "7dRtFA8vRLHZTPdMyHKX+P",
+            "86EAAucRBNYr7QmR3FS+Pq",
+            "8c20Sso/ZEn7NUfNSM+EBh",
+            "93Aa/GpthIOp+KXiM1Lm/G",
+            "94HhzSGYlMWLCTRNZ7euWO",
+            "96BF9/lUdFopk8Np2Yw5Zx",
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            "a56XoNOhVDmIJSFU8+yxyb",
+            "ad5jgcQZtPjbAjHnXTqJ7R",
+            "afIQ4nqr9A65H1RkOCdAJl",
+            "b1FbVxPhlN+L+Y83MXzy3d",
+            "b2qinzHe1IY6RV8Z3Zn4Fg",
+            "b5GEfhdC1D2Ja3Hj1/6uuj",
+            "b7klSoZEJIzppdHYoz8uWx",
+            "bdc5JlokZIxqWVSH30sitG",
+            "d0buogR4hFtpjOE1oGYJUs",
+            "dcrmB6BMFPjICnwOJWmcFW",
+            "e03WupkUNKQYo70omLdH2z",
+            "f647AQtCpIcpnJ4iRz5L7m",
+            "f7oDfGf2lJYp7H66FM+tWx",
+            "fbklrM/SJK0JttJmEe70M9",
+            "fcgDuFQNNDBZUTTmD8QHSZ",
+            "fflzOJSGtFi7dOYB/GebuI"
+        ]
+    },
+    orientation: "landscape",
+    uuids: [
+        "d3e8uZgOdC1pKPDaNBNFcU",
+        "41ywKJsOBHOKb/3Qi6DGP5",
+        "73WnM0X1RDyYZgcYIqEVbu"
+    ],
+    "md5AssetsMap": {
+        "04/04defaae1.json": "615ef",
+        "0f/0f0998c25.json": "1ffa7",
+        "0f/0f4cb3be0.json": "f7d43",
+        "b7/b79254a8-6442-48ce-9a5d-1d8a33f2e5b1/word.ttf": "2ebf4",
+        "assets/res/audio/Bgsound1.mp3": "75f83",
+        "assets/res/audio/Bgsound2.mp3": "1142a",
+        "assets/res/audio/Bgsound3.mp3": "470a5",
+        "assets/res/audio/Bgsound4.mp3": "8d783",
+        "assets/res/audio/Boomb.mp3": "c1955",
+        "assets/res/audio/Btnsound.mp3": "79289",
+        "assets/res/audio/Failsound.mp3": "7f19e",
+        "assets/res/audio/Levelup.mp3": "da59b",
+        "assets/res/audio/normal-value.mp3": "8f35e",
+        "assets/res/audio/score1.mp3": "4d390",
+        "assets/res/audio/Startsound.mp3": "ebddb",
+        "assets/res/gamesellect/menu0.png": "30b0d",
+        "assets/res/gamesellect/menu1.png": "28a1b",
+        "assets/res/gamesellect/menu2.png": "da1e4",
+        "assets/res/gamesellect/menu3.png": "d1bc0",
+        "assets/res/gamesellect/tu0.png": "29a22",
+        "assets/res/gamesellect/tu1.png": "80775",
+        "assets/res/gamesellect/tu2.png": "d86cb",
+        "assets/res/gamesellect/tu3.png": "bcd57",
+        "assets/res/gamesellect/xiangyou1.png": "6e330",
+        "assets/res/gamesellect/xiangyou2.png": "eb67a",
+        "assets/res/gamesellect/xiangzuo1.png": "70879",
+        "assets/res/gamesellect/xiangzuo2.png": "5e1d3",
+        "assets/res/gamesellect/xuanguanbeijing.png": "aa439",
+        "assets/res/gamestart/background.png": "77cc1",
+        "assets/res/gamestart/fanhui1.png": "a52cd",
+        "assets/res/gamestart/fanhui2.png": "b2a7c",
+        "assets/res/gamestart/kaishi1.png": "bf042",
+        "assets/res/gamestart/kaishi2.png": "75804",
+        "assets/res/gamestart/rope.png": "e7508",
+        "assets/res/pre/fish-0.png": "5625a",
+        "assets/res/pre/fish-1.png": "fed1e",
+        "assets/res/pre/fish-2.png": "0bb20",
+        "assets/res/pre/fish-3.png": "b3ff8",
+        "assets/res/pre/fish-4.png": "a6ee5",
+        "assets/res/pre/fish-5.png": "77166",
+        "assets/res/pre/gem.png": "09c43",
+        "assets/res/pre/shoes-0.png": "90c25",
+        "assets/res/pre/shoes-1.png": "e44a3",
+        "assets/res/pre/stone-0.png": "38efd",
+        "assets/res/pre/stone-1.png": "5b66b",
+        "assets/res/pre/stone-2.png": "13fa8",
+        "assets/res/pre/stone-3.png": "a2f5e",
+        "assets/res/tustart/bg-0.png": "fd8e7",
+        "assets/res/tustart/bg-1.png": "a1941",
+        "assets/res/tustart/bg-2.png": "e642d",
+        "assets/res/tustart/bg-3.png": "ff532",
+        "assets/res/tustart/boom.png": "bc58d",
+        "assets/res/tustart/Btnfram.png": "d6e5e",
+        "assets/res/tustart/Btnfram2.png": "92fed",
+        "assets/res/tustart/framework1.png": "e8d72",
+        "assets/res/tustart/hook.png": "80dab",
+        "assets/res/tustart/menu.png": "4dc35",
+        "assets/res/tustart/menu2.png": "67b14",
+        "assets/res/tustart/pause.png": "09ebe",
+        "assets/res/tustart/pause2.png": "7ddbc",
+        "assets/res/tustart/progress.png": "f4a7a",
+        "assets/res/tustart/restart.png": "dc230",
+        "assets/res/tustart/restart2.png": "902f1",
+        "assets/res/tustart/ship1.png": "de39d",
+        "assets/res/tustart/ship2.png": "8bb43",
+        "assets/res/tustart/xiegan.png": "90d16",
+        "internal/image/default_sprite_splash.png": "cea68",
+        "internal/image/default_sprite.png": "68270"
+    }
+};
